9432512b71704942c7d8c163a7ef9e2b9ee59c68
Alexis <alexis52gamer@gmail.com>
2026-05-09 21:10:17 -0600

    Add files via upload

---
+++ programacion.js
@@ -0,0 +1,368 @@
+const preguntas = [
+
+{
+pregunta:
+"¿Qué lenguaje se usa para páginas web?",
+
+opciones:[
+"HTML",
+"Photoshop",
+"Excel",
+"Unity"
+],
+
+correcta:"HTML"
+},
+
+{
+pregunta:
+"¿Qué hace CSS?",
+
+opciones:[
+"Da diseño",
+"Hace música",
+"Crea videos",
+"Apaga PC"
+],
+
+correcta:"Da diseño"
+},
+
+{
+pregunta:
+"¿Qué lenguaje da lógica a una web?",
+
+opciones:[
+"JavaScript",
+"Paint",
+"Word",
+"PNG"
+],
+
+correcta:"JavaScript"
+},
+
+{
+pregunta:
+"¿Qué motor usa C#?",
+
+opciones:[
+"Unity",
+"Roblox",
+"Chrome",
+"Android"
+],
+
+correcta:"Unity"
+},
+
+{
+pregunta:
+"¿Qué significa bug?",
+
+opciones:[
+"Error",
+"Virus",
+"Mouse",
+"Pantalla"
+],
+
+correcta:"Error"
+}
+
+];
+
+/* ELEMENTOS */
+
+const preguntaTexto =
+document.getElementById("pregunta");
+
+const opcionesHTML =
+document.querySelector(".options");
+
+const feedback =
+document.querySelector(".feedback");
+
+const xpText =
+document.getElementById("xp");
+
+const nextBtn =
+document.querySelector(".next-btn");
+
+const questionNumber =
+document.querySelector(".question-number");
+
+const vidasHTML =
+document.querySelector(".vidas");
+
+/* VARIABLES */
+
+let preguntaActual = 0;
+
+let xp = 0;
+
+let vidas = 3;
+
+let respondido = false;
+
+let puntosGuardados = false;
+
+/* VIDAS */
+
+function actualizarVidas(){
+
+vidasHTML.innerHTML = "";
+
+for(let i=0;i<vidas;i++){
+
+vidasHTML.innerHTML += "❤️ ";
+
+}
+
+}
+
+/* MEZCLAR */
+
+function mezclarArray(array){
+
+for(let i=array.length-1;i>0;i--){
+
+const j =
+Math.floor(Math.random()*(i+1));
+
+[array[i],array[j]] =
+[array[j],array[i]];
+
+}
+
+return array;
+
+}
+
+/* CARGAR */
+
+function cargarPregunta(){
+
+respondido = false;
+
+feedback.style.display = "none";
+
+const actual =
+preguntas[preguntaActual];
+
+questionNumber.textContent =
+`Pregunta ${preguntaActual+1} de ${preguntas.length}`;
+
+preguntaTexto.textContent =
+actual.pregunta;
+
+opcionesHTML.innerHTML = "";
+
+const opcionesMezcladas =
+mezclarArray([...actual.opciones]);
+
+opcionesMezcladas.forEach(opcion=>{
+
+const boton =
+document.createElement("button");
+
+boton.classList.add("option");
+
+boton.textContent = opcion;
+
+boton.addEventListener("click",()=>{
+
+if(respondido) return;
+
+respondido = true;
+
+if(opcion===actual.correcta){
+
+boton.style.background="#10b981";
+
+boton.style.color="white";
+
+xp += 10;
+
+xpText.textContent = xp;
+
+feedback.style.display="block";
+
+feedback.innerHTML =
+"✅ ¡Correcto!";
+
+feedback.style.background="#dcfce7";
+
+feedback.style.color="#166534";
+
+}
+
+else{
+
+vidas--;
+
+actualizarVidas();
+
+boton.style.background="#ef4444";
+
+boton.style.color="white";
+
+feedback.style.display="block";
+
+feedback.innerHTML =
+"❌ Incorrecto";
+
+feedback.style.background="#fee2e2";
+
+feedback.style.color="#991b1b";
+
+const botones =
+document.querySelectorAll(".option");
+
+botones.forEach(btn=>{
+
+if(
+btn.textContent===
+actual.correcta
+){
+
+btn.style.background="#10b981";
+
+btn.style.color="white";
+
+}
+
+});
+
+if(vidas<=0){
+
+xp=0;
+
+xpText.textContent = xp;
+
+setTimeout(()=>{
+
+terminarQuiz(false);
+
+},1000);
+
+}
+
+}
+
+});
+
+opcionesHTML.appendChild(boton);
+
+});
+
+}
+
+/* CONTINUAR */
+
+nextBtn.addEventListener("click",()=>{
+
+if(!respondido) return;
+
+preguntaActual++;
+
+if(
+preguntaActual>=preguntas.length
+){
+
+terminarQuiz(true);
+
+return;
+
+}
+
+cargarPregunta();
+
+});
+
+/* TERMINAR */
+
+function terminarQuiz(gano){
+
+opcionesHTML.innerHTML="";
+
+feedback.style.display="block";
+
+if(gano){
+
+let puntosActuales =
+
+parseInt(
+localStorage.getItem("puntos")
+)||0;
+
+if(!puntosGuardados){
+
+puntosActuales += xp;
+
+localStorage.setItem(
+"puntos",
+puntosActuales
+);
+
+puntosGuardados=true;
+
+}
+
+preguntaTexto.textContent =
+"🎉 ¡Quiz completado!";
+
+feedback.innerHTML =
+`
+🏆 XP: ${xp}
+<br><br>
+⭐ Total:
+${puntosActuales}
+`;
+
+feedback.style.background="#dcfce7";
+
+feedback.style.color="#166534";
+
+}
+
+else{
+
+preguntaTexto.textContent =
+"💀 Te quedaste sin vidas";
+
+feedback.innerHTML =
+"❌ No ganaste puntos";
+
+feedback.style.background="#fee2e2";
+
+feedback.style.color="#991b1b";
+
+}
+
+nextBtn.innerHTML =
+"🏠 Regresar";
+
+nextBtn.onclick = ()=>{
+
+window.location.href =
+"loggin.html";
+
+};
+
+}
+
+/* SALIR */
+
+function salirQuiz(){
+
+window.location.href =
+"loggin.html";
+
+}
+
+/* INICIAR */
+
+actualizarVidas();
+
+cargarPregunta();
