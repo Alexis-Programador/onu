df61e4b355403fc367e2e9ed150b279f500d37d8
Alexis <alexis52gamer@gmail.com>
2026-05-09 21:25:35 -0600

    Update ingles.js

--- ingles.js
+++ ingles.js
@@ -313,6 +313,13 @@ function terminarQuiz(gano){
 
     if(gano){
 
+         /* MARCAR QUIZ COMPLETADO */
+
+    localStorage.setItem(
+    "quizCompletado",
+    "true"
+    );
+        
         let puntosActuales =
 
         parseInt(
